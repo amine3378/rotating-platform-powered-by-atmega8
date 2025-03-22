94c8b0d619feca984c34115bce04ea260d2cc03c
amine3378 <89049093+amine3378@users.noreply.github.com>
2025-03-22 02:52:34 +0100

    Add files via upload

---
+++ lcd.h
@@ -0,0 +1,25 @@
+#ifndef LCD_H
+#define LCD_H
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define LCD_Data_Dir DDRD         /* Define LCD data port direction */
+#define LCD_Command_Dir DDRC      /* Define LCD command port direction register */
+#define LCD_Data_Port PORTD       /* Define LCD data port */
+#define LCD_Command_Port PORTC    /* Define LCD data port */
+#define RS PC0                    /* Define Register Select (data/command reg.) pin */
+#define RW PC1                    /* Define Read/Write signal pin */
+#define EN PC2                    /* Define Enable signal pin */
+
+#define DELAY  500               /* milliseconds between steps */
+
+/* Function Declarations */
+void LCD_Command(unsigned char cmnd);
+void LCD_Char(unsigned char char_data);
+void LCD_Init(void);
+void LCD_String(char *str);
+void LCD_String_xy(char row, char pos, char *str);
+void LCD_Clear(void);
+
+#endif
---
+++ main.c
@@ -0,0 +1,47 @@
+#include <avr/io.h>			/* Include AVR std. library file */
+#include <util/delay.h>			/* Include inbuilt defined Delay header file */
+#include "lcd.h"  
+
+#define BLUE     _BV(PB0)
+#define BLACK    _BV(PB1)
+#define RED      _BV(PB2)
+#define YELLOW   _BV(PB3)
+
+#define DELAY 60` /* milliseconds between steps */
+
+int main(void)
+{
+    DDRB = 0xff;      /* Set PORTB as output */
+    PORTB = 0x00;     /* Initialize PORTB to 0 */
+    LCD_Init();       /* Initialize the LCD */
+    
+    LCD_String_xy(0, 0, "eswara jamila");   /* Display string on the first line of LCD */
+    
+    while(1)  /* Infinite loop to control the LED states */
+    {
+        PORTB = BLUE;
+        _delay_ms(DELAY); 
+        
+        PORTB = BLUE | BLACK;
+        _delay_ms(DELAY);       
+        
+        PORTB = BLACK;
+        _delay_ms(DELAY);        
+        
+        PORTB = RED | BLACK;
+        _delay_ms(DELAY);
+        
+        PORTB = RED;
+        _delay_ms(DELAY);
+        
+        PORTB = RED | YELLOW;
+        _delay_ms(DELAY);
+        
+        PORTB = YELLOW;
+        _delay_ms(DELAY);
+        
+        PORTB = YELLOW | BLUE;
+        _delay_ms(DELAY);
+    }
+}
+ 
